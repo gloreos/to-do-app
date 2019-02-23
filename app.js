27f4671f889d85aa85c2cd16ad779898e2890df8
gloreos <choi.gloria99@gmail.com>
2019-02-23 15:28:22 -0800

    Checkpoint 6 events2: completed

--- app.js
+++ app.js
@@ -1,54 +1,45 @@
 function onReady() {
-     const addToDoForm = document.getElementById("addToDoForm");
-     const newToDoText = document.getElementById("newToDoText");
-     const toDoList = document.getElementById("toDoList");
+  const toDos = [];
+  const addToDoForm = document.getElementById("addToDoForm");
 
-     addToDoForm.addEventListener("submit", () => {
-       event.preventDefault();
+  function createNewToDo() {
+    const newToDoText = document.getElementById("newToDoText");
+    if (!newToDoText.value) { return; }
 
-       //get the text
-       let title = newToDoText.value;
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
 
-       //create a new li
-       let newLi = document.createElement("li");
-       //style the newLi
-       newLi.classList.add("mdl-list__item");
+    newToDoText.value = " ";
+    
+    renderTheUI();
+  }
 
-       //create a new input
-       let checkbox = document.createElement("input");
-       //set the input's type to checkbox
-       checkbox.type = "checkbox";
-       //style the checkbox
-       checkbox.style.margin = "0 5px 0 0";
-       //attach the checkbox to the li
-       newLi.appendChild(checkbox);
 
-       //set the title
-       newLi.innerHTML += title;
+  function renderTheUI() {
+    const toDoList = document.getElementById("toDoList");
 
-       //create a delete button
-       let deleteToDo = document.createElement("button");
-       //set the button text
-       deleteToDo.textContent = "Delete";
-       //style the button
-       deleteToDo.style.margin = "0 0 0 20px";
-       deleteToDo.classList.add("mdl-button", "mdl-js-button", "mdl-button--raised", "mdl-button--accent");
-       //attach the button to the li
-       newLi.appendChild(deleteToDo);
+    toDoList.textContent = " ";
 
-       //attach the li to the ul
-       toDoList.appendChild(newLi);
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement("li");
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
 
-       //empty the input
-       newToDoText.value = '';
+      newLi.textContent = toDo.title;
 
-       //delete the newLi
-       deleteToDo.addEventListener("click", () => {
-         toDoList.removeChild(newLi);
-       });
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
 
-     });
+  addToDoForm.addEventListener("submit", event => {
+    event.preventDefault();
+    createNewToDo();
+  });
 
+  renderTheUI();
  }
 
  window.onload = function() {
